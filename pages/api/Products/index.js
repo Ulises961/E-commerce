4216906458a4a48601b702b1342d8d084df47a62
Sosa Ulises Emiliano <ulisesemiliano.sosa@eurac.edu>
2022-01-16 01:39:27 +0100

    insert middlewares for connection with mongo, seed db, and create an entry point for getting all items

---
+++ pages/api/Products/index.js
@@ -0,0 +1,12 @@
+import nextConnect from 'next-connect';
+import Product from '../../../models/Product';
+import db from '../../../utils/db';
+const handler = nextConnect();
+
+handler.get(async (req, res) => {
+  await db.connect();
+  const products = await Product.find({});
+  db.disconnect();
+  res.send(products);
+});
+export default handler;
