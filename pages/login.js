bbb981ac6f6da3e174da92a0e547b8b87ab32187
Sosa Ulises Emiliano <ulisesemiliano.sosa@eurac.edu>
2022-02-01 01:02:00 +0100

    implement login function, and backend authentication

--- pages/login.js
+++ pages/login.js
@@ -1,14 +1,26 @@
 import { Button, List, ListItem, TextField, Typography } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../components/Layout';
 import useStyles from '../utils/styles';
 import NextLink from 'next/link';
 import { Link } from '@mui/material';
+import axios from 'axios';
+
 export default function Login() {
   const classes = useStyles();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const submitHandler = async (e) => {
+  try{  e.preventDefault();
+    const { data } = await axios.post('/api/users/login', { email, password });}
+    catch(err){
+      alert(err.response.data? err.response.data.message : err.message);
+    }
+  };
   return (
     <Layout title="Login">
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <Typography component="h1" variant="h1">
           Login
         </Typography>
@@ -16,31 +28,26 @@ export default function Login() {
           <ListItem className="form">
             <TextField
               variant="outlined"
-              outlined
               id="email"
               label="Email"
               fullWidth={true}
               inputProps={{ type: 'email' }}
+              onChange={(e) => setEmail(e.target.value)}
             ></TextField>
           </ListItem>
           <ListItem>
             <TextField
               variant="outlined"
-              outlined
               id="password"
               label="Password"
               fullWidth={true}
               inputProps={{ type: 'password' }}
+              onChange={(e) => setPassword(e.target.value)}
             ></TextField>
           </ListItem>
 
           <ListItem>
-            <Button
-              variant="contained"
-              type="submit"
-              fullWidth
-              color="primary"
-            >
+            <Button variant="contained" type="submit" fullWidth color="primary">
               Login
             </Button>
           </ListItem>
